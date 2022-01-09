7f74fa94afbd66193fbaae41fdd7c2d1a5cd30e7
David Sciacchettano <david.sciacchettano@gmail.com>
2022-01-09 17:00:50 -0500

    Create todo api gateway health entry

--- app/infra/__main__.py
+++ app/infra/__main__.py
@@ -9,9 +9,24 @@ import pulumi_aws as aws
 # # Export the name of the bucket
 # pulumi.export('bucket_name', bucket.id)
 
-# Create a RestAPI
-
+# Create a RestAPI for the health path
 todo_rest_api = aws.apigateway.RestApi(
     "TodoRestApi",
     description="This is the API Gateway used for the Todo app"
+)
+# Create a Resource to put in the API Gateway
+todo_rest_resource = aws.apigateway.Resource(
+    "healthResource",
+    rest_api=todo_rest_api.id,
+    parent_id=todo_rest_api.root_resource_id,
+    path_part="health"
+)
+
+# Create a Method for the health path
+todo_health_method = aws.apigateway.Method(
+    "healthMethod",
+    rest_api=todo_rest_api.id,
+    resource_id=todo_rest_resource.id,
+    http_method="GET",
+    authorization="NONE"
 )
