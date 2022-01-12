dc01f8979e3b88f10f3033e46a5068207c3899a2
David Sciacchettano <david.sciacchettano@gmail.com>
2022-01-12 17:56:11 -0500

    Create the base setup of the handler

--- app/infra/__main__.py
+++ app/infra/__main__.py
@@ -1,10 +1,42 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# # Create an AWS resource (S3 Bucket)
+# bucket = s3.Bucket('my-bucket')
+#
+# # Export the name of the bucket
+# pulumi.export('bucket_name', bucket.id)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Create Health AWS Lambda
+iam_for_health_lambda=aws.iam.Role(
+    "iamHealthLambda",
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts.AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""             
+            }
+        ]
+    }
+    """
+)
+
+health_lambda = aws.lambda_.Function(
+    "healthLambda",
+    code=pulumi.FileArchive(""),
+    role=iam_for_health_lambda.arn,
+    handler="app.lambda.handlers",
+    runtime="python3.8",
+    environment=aws.lambda_.FunctionEnvironmentArgs(
+        variables={
+            "foo": "bar"
+        }
+    )
+)
