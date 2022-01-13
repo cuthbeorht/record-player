e0d35fffc6481d3b52cc89b956d0b3eb9d1f10d4
David Sciacchettano <david.sciacchettano@gmail.com>
2022-01-12 20:04:38 -0500

    Add health lambda

--- app/infra/__main__.py
+++ app/infra/__main__.py
@@ -3,20 +3,14 @@
 import pulumi
 import pulumi_aws as aws
 
-# # Create an AWS resource (S3 Bucket)
-# bucket = s3.Bucket('my-bucket')
-#
-# # Export the name of the bucket
-# pulumi.export('bucket_name', bucket.id)
-
 # Create Health AWS Lambda
-iam_for_health_lambda=aws.iam.Role(
+iam_for_health_lambda = aws.iam.Role(
     "iamHealthLambda",
     assume_role_policy="""{
         "Version": "2012-10-17",
         "Statement": [
             {
-                "Action": "sts.AssumeRole",
+                "Action": "sts:AssumeRole",
                 "Principal": {
                     "Service": "lambda.amazonaws.com"
                 },
@@ -30,7 +24,7 @@ iam_for_health_lambda=aws.iam.Role(
 
 health_lambda = aws.lambda_.Function(
     "healthLambda",
-    code=pulumi.FileArchive(""),
+    code=pulumi.FileArchive("../../dist/lambda.zip"),
     role=iam_for_health_lambda.arn,
     handler="app.lambda.handlers",
     runtime="python3.8",
