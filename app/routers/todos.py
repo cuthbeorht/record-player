3629a50e03c5f46c839b2f3919ce8a880c29631a
David <cuthbeorht@users.noreply.github.com>
2022-02-06 21:02:22 -0500

    Add stubbed POST controller endpoint (#19)

--- app/routers/todos.py
+++ app/routers/todos.py
@@ -1,9 +1,9 @@
+import datetime
+
 from fastapi import APIRouter, Depends, Response
 
-from app.database import DatabaseConnection
 from app.services.todos import Service as TodoService, TodoBase
-from app.database import DatabaseConnection
-from typing import List
+from typing import List, Any
 from pydantic import BaseModel
 from app.dependencies import todo_service
 
@@ -12,6 +12,21 @@ router = APIRouter()
 
 class GetTodosResponse(BaseModel):
     todos: List[TodoBase]
+
+
+class CreateTodoRequest(BaseModel):
+    title: str
+    description: str
+    created_by: str
+    created: Any = datetime.datetime.utcnow()
+
+
+class CreateTodoResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    created_by: str
+    created: Any = datetime.datetime.utcnow()
 
 
 @router.get("/{id}")
@@ -24,3 +39,21 @@ async def get_todos(todo_service: TodoService = Depends(todo_service)) -> GetTod
     todos = await todo_service.get_todos()
 
     return GetTodosResponse(**{"todos": todos})
+
+
+@router.post(
+    "/",
+    response_model=CreateTodoResponse,
+    status_code=201
+)
+async def create_todo_item(
+    request: CreateTodoRequest,
+    todo_service: TodoService = Depends(todo_service)
+) -> CreateTodoResponse:
+    return CreateTodoResponse(
+        id=1,
+        title="The Title",
+        description="This is a crappy description",
+        created_by="skjfkj",
+        created=datetime.datetime.utcnow()
+    )
