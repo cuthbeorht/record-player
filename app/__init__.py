34201fae8b0f28197fde41661a991ddf174f9bc6
David Sciacchettano <davidsciacchettano@exfo.com>
2022-04-10 15:20:38 -0400

    Add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,21 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.health import router as health_router
 from app.routers.todos import router as todo_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_methods=['*'],
+        allow_headers=['*'],
+        allow_credentials=True
+    )
 
     app.include_router(health_router, prefix="/health")
     app.include_router(todo_router, prefix="/todos")
